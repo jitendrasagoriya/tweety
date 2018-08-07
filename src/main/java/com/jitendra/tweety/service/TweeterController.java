765ca56d209702322c1b07681272c244ca0f3d79
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-08-07 10:32:55 +0530

    add get request

--- src/main/java/com/jitendra/tweety/service/TweeterController.java
+++ src/main/java/com/jitendra/tweety/service/TweeterController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
