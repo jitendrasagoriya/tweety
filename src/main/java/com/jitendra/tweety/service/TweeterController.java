b259678e09706d17b5479a0d0d578f77efcf0c4d
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-09-08 10:24:45 +0530

    Add CROS

--- src/main/java/com/jitendra/tweety/service/TweeterController.java
+++ src/main/java/com/jitendra/tweety/service/TweeterController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import twitter4j.TwitterException;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path="/api/tweety/")
 public class TweeterController {
